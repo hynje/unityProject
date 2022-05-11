98dd18478aa49aa42620e4213841ab804426bee3
ckdud9299 <ckdud9299@naver.com>
2022-05-11 23:52:46 +0900

    구글로그인 db와 연동

---
+++ Project/Assets/Scripts/LoginManager.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Google;
+using Firebase.Auth;
+using System.Threading.Tasks;
+
+using UnityEngine.SceneManagement;
+
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity;
+
+public class LoginManager : MonoBehaviour
+{
+    // Auth 용 instance
+    FirebaseAuth auth = null;
+
+    // 사용자 계정
+    FirebaseUser user = null;
+
+    // 데이터를 쓰기 위한 reference 변수
+    DatabaseReference reference;
+
+    // 기기 연동이 되어 있는 상태인지 체크한다.
+    private bool signedIn = false;
+
+    private void Awake()
+    {
+        // 초기화
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        // 유저의 로그인 정보에 어떠한 변경점이 생기면 실행되게 이벤트를 걸어준다.
+        auth.StateChanged += AuthStateChanged;
+        //AuthStateChanged(this, null);
+    }
+
+    // 계정 로그인에 어떠한 변경점이 발생시 진입.
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            // 연동된 계정과 기기의 계정이 같다면 true를 리턴한다. 
+            signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+
+            if (!signedIn && user != null)
+            {
+                UnityEngine.Debug.Log("Signed out " + user.UserId);
+            }
+
+            user = auth.CurrentUser;
+
+            if (signedIn)
+            {
+                UnityEngine.Debug.Log("Signed in " + user.UserId);
+            }
+        }
+    }
+
+    // 구글 로그인 //
+    public void GoogleLoginProcessing()
+    {
+        if (GoogleSignIn.Configuration == null)
+        {
+            // 설정
+            GoogleSignIn.Configuration = new GoogleSignInConfiguration
+            {
+                RequestIdToken = true,
+                RequestEmail = true,
+                WebClientId = "856468702829-mke0pse0oe8b7bod29e2941osvhia83d.apps.googleusercontent.com"
+            };
+        }
+
+        Task<GoogleSignInUser> signIn = GoogleSignIn.DefaultInstance.SignIn();
+
+        TaskCompletionSource<FirebaseUser> signInCompleted = new TaskCompletionSource<FirebaseUser>();
+
+        signIn.ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.Log("Google Login task.IsCanceled");
+            }
+            else if (task.IsFaulted)
+            {
+                Debug.Log("Google Login task.IsFaulted");
+            }
+            else
+            {
+                Credential credential = Firebase.Auth.GoogleAuthProvider.GetCredential(((Task<GoogleSignInUser>)task).Result.IdToken, null);
+                auth.SignInWithCredentialAsync(credential).ContinueWith(authTask =>
+                {
+                    if (authTask.IsCanceled)
+                    {
+                        signInCompleted.SetCanceled();
+                        Debug.Log("Google Login authTask.IsCanceled");
+                        return;
+                    }
+                    if (authTask.IsFaulted)
+                    {
+                        signInCompleted.SetException(authTask.Exception);
+                        Debug.Log("Google Login authTask.IsFaulted");
+                        return;
+                    }
+
+                    user = authTask.Result;
+                    Debug.LogFormat("Google User signed in successfully: {0} ({1})", user.DisplayName, user.UserId);
+
+                    Firebase.Auth.FirebaseUser newUser = authTask.Result;
+                    writeNewUser(newUser.UserId, newUser.DisplayName);
+
+                    SceneManager.LoadScene("MainScene");
+
+                    return;
+                });
+            }
+        });
+    }
+
+    // 연동 해제
+    public void SignOut()
+    {
+        if (auth.CurrentUser != null)
+            auth.SignOut();
+    }
+
+    // 연동 계정 삭제
+    public void UserDelete()
+    {
+        if (auth.CurrentUser != null)
+            auth.CurrentUser.DeleteAsync();
+    }
+
+    public class User
+    {
+        public string username;
+        public bool tutorial_state;
+        public int highscore;
+        public int score;
+
+        public User() {
+        }
+
+        public User(string username, bool tutorial_state, int highscore, int score)
+        {
+            this.username = username;
+            this.tutorial_state = false;
+            this.highscore = highscore;
+            this.score = score;
+        }
+    }
+
+    void writeNewUser(string userId, string username)
+    {
+        User user = new User();
+        string json = JsonUtility.ToJson(user);
+        reference.Child(userId).SetRawJsonValueAsync(json);
+        reference.Child(userId).Child("username").SetValueAsync(username);
+
+    }
+
+}
